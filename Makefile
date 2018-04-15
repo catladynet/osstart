cbdf20a52fbdc8054b8c965e9acfcb0a33a2d29a
OuyangZhuaibai <450976770@qq.com>
2018-04-15 12:16:03 +0800

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-bootloader.bin: start.s
-	gcc -c -m32 start.s -o start.o
+start.img: start.s
+	#编译生成.o文件
+	gcc -c -m32 start.s -o start.o								 
+	#将start.s文件中标志为start代码放在0x7c00处开始，生成elf格式文件	 
 	ld -m elf_i386 -e start -Ttext 0x7c00 start.o -o start.elf
-	objcopy -S -j .text -O binary start.elf start.bin
-	cat start.bin > start.img
+	#编译生成.bin文件
+	objcopy -S -j .text -O binary start.elf start.bin			 
+	#得到img镜像文件，用于启动加载
+	cat start.bin > start.img									 
 
 clean:
 	rm -rf *.o *.elf *.bin *.img
--- start.s
+++ start.s
@@ -2,10 +2,9 @@
 .global start
 .code16
 start:
-        movw $0x0000, %ax    #屏幕清零
+        movw $0x0000, %ax     #屏幕清零
         int  $0x10
-        movw $message, %bp
-        #movw %ax, %bp         #字符串地址传送给bp
+        movw $message, %bp    #字符串地址传送给bp
         movw $13, %cx         #输出的消息有13个字符      
         movw $0x1301, %ax     #AH=0x13 打印字符串                  
         movw $0x000c, %bx     #BH=0x00 黑底 BL=0x0c 红字           
@@ -16,5 +15,5 @@ l:      hlt
 message: 
         .string "Hello, World!\0"
 #.org new-lc,fill:从new-lc标识的新位置开始存放下边的代码或数据，之前空出来的空间用fill填充
-.org  0x1fe,0x90
-.word 0xaa55
+.org  0x1fe,0x90              #从第511个字节开始
+.word 0xaa55                  #填充55和aa
