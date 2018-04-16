80e19accdf7942094e04d15b720feb095f9caa86
sunruianon <450976770@qq.com>
2018-04-16 22:00:17 +0800

    1

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # osstart
-
 a simple os boot sector<br>
 在终端下运行：<br>
-~/working/ost$ make<br>
+~/working/ostart$ make<br>
 然后运行：<br>
-~/working/ost$ make play<br>
+~/working/ostart$ make play<br>
 即可展示一个最简单的操作系统<br>
 其实这只是一个实模式下的引导扇区（boot sector）<br>
 
 #### 关于makefile：
-
 maekfile文件是终端命令的一个集合，帮助我们批量编译程序<br>
 观察本次项目中的makefile文件，写了四个操作<br>
 终端使用命令make执行第一个操作<br>
@@ -17,7 +15,8 @@ maekfile文件是终端命令的一个集合，帮助我们批量编译程序<br
 以此类推，make clean就是清除缓存等等<br>
 
 #### 关于AT&T汇编和Intel汇编
-
 本篇采用AT&T汇编，其为GNU/linux世界通用标准，文件后缀.s，可以用gcc直接编译<br>
-但是网上大量汇编参考代码为Intel标准，适合在windows下开发<br>
+但是网上大量汇编参考代码为Intel标准，文件后缀.asm适合在windows下开发<br>
 两者语法有大量不同之处，熟练后可以相互转化<br>
+
+#### 关于
--- start.s
+++ start.s
@@ -1,9 +1,9 @@
-.text
-.global start
-.code16
+.text                         #指明代码段
+.global start                 #指明start段为全局段，可被其他找到
+.code16                       #指明16位代码（实模式）
 start:
         movw $0x0000, %ax     #屏幕清零
-        int  $0x10
+        int  $0x10            #中断调用
         movw $message, %bp    #字符串地址传送给bp
         movw $13, %cx         #输出的消息有13个字符      
         movw $0x1301, %ax     #AH=0x13 打印字符串                  
@@ -17,3 +17,5 @@ message:
 #.org new-lc,fill:从new-lc标识的新位置开始存放下边的代码或数据，之前空出来的空间用fill填充
 .org  0x1fe,0x90              #从第511个字节开始
 .word 0xaa55                  #填充55和aa
+#经过上面两行代码构造了一个完整的启动扇区
+#bios识别启动磁盘第一个扇区（前512字节），最后两个字节为55和aa，则标志为一个可加载boot
